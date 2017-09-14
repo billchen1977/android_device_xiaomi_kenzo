1ebf906d239b3018150d72d94ff59ad63b54504d
cb <billchen1977@gmail.com>
2017-09-14 14:38:38 +0800

    Move all executables to vendor
    Change-Id: Ia905073f6785e5e6fe617725ca46572a06fa6783

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -95,6 +95,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 include $(BUILD_PREBUILT) 
 
@@ -102,6 +103,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.fm.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES    := etc/init.qcom.fm.sh
 include $(BUILD_PREBUILT)
 
@@ -109,6 +111,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
 include $(BUILD_PREBUILT)
 
@@ -116,5 +119,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.zram.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES    := etc/init.qcom.zram.sh
 include $(BUILD_PREBUILT)

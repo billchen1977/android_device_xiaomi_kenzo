963d20fd9294cd395e87540f7e5b4b932c6f1901
David <Davidteri91@gmail.com>
2017-09-17 18:59:18 +0800

    add configstore service needed by surfaceflinger and VSYNC read: https://android.googlesource.com/platform/hardware/interfaces/+/200825e359f7481f0916c61f1d55a718a39f457b
    Conflicts:
    	device.mk
    
    Change-Id: Ida07ff3ee4de4eafa577b739ee305c2b98289375

--- device.mk
+++ device.mk
@@ -156,6 +156,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbt-vendor
 
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
